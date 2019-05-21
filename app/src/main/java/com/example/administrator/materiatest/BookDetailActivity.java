4434afb140709b540d3123d002e6fdfef172b675
Carmen <654813926@qq.com>
2019-05-21 20:01:27 +0800

    解决冲突

---
+++ app/src/main/java/com/example/administrator/materiatest/BookDetailActivity.java
@@ -0,0 +1,41 @@
+package com.example.administrator.materiatest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class BookDetailActivity extends AppCompatActivity {
+    public static String Intent_Book_ToEdit = "BOOKTOEDIT";
+    private static final String TAG = "BookDetailActivity";
+    //private Book mBook;
+
+    private TextView infoTitleTextView;
+    private ImageView coverImageView;
+    private TextView addtimeTextView;
+    private RelativeLayout authorRelativeLayout;
+    private TextView authorTextView;
+    private RelativeLayout translatorRelativeLayout;
+    private TextView translatorTextView;
+    private RelativeLayout publisherRelativeLayout;
+    private TextView publisherTextView;
+    private RelativeLayout pubtimeRelativeLayout;
+    private TextView pubtimeTextView;
+    private RelativeLayout isbnRelativeLayout;
+    private TextView isbnTextView;
+    private RelativeLayout readingStatusRelativeLayout;
+    private TextView readingStatusTextView;
+    private RelativeLayout bookshelfRelativeLayout;
+    private TextView bookshelfTextView;
+    private RelativeLayout notesRelativeLayout;
+    private TextView notesTextView;
+    private RelativeLayout labelsRelativeLayout;
+    private TextView labelsTextView;
+    private RelativeLayout websiteRelativeLayout;
+    private TextView websiteTextView;
+
+
+
+}
