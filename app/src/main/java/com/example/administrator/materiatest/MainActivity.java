11e8f9d4d35c9a99311943eda55aaa58bf910ee6
Carmen <654813926@qq.com>
2019-06-07 15:22:10 +0800

    标签修改

--- app/src/main/java/com/example/administrator/materiatest/MainActivity.java
+++ app/src/main/java/com/example/administrator/materiatest/MainActivity.java
@@ -2,27 +2,44 @@ package com.example.administrator.materiatest;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.Toast;
-import com.getbase.floatingactionbutton.*;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawLayout;
     final String[] items4 = new String[]{"标题", "作者", "出版社", "出版时间"};
+    EditText temp_tagText;
+    public NavigationView navigationView;
+    public ListView nav_labels_lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +52,31 @@ public class MainActivity extends AppCompatActivity {
         mDrawLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
 
-        NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
-
+        navigationView=(NavigationView)findViewById(R.id.nav_view);
+        nav_labels_lv=(ListView)findViewById(R.id.label_holder);
 
        if(actionBar!=null){
            actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
-        //测拉菜单的
+
+
+        //侧拉菜单的
         navigationView.setCheckedItem(R.id.nav_book);
+
+        final List<Label> labels = LabelLab.get(this).getLabels();
+        for (int i = 0; i < labels.size(); i++) {
+            // add labels
+            IDrawerItem drawerItem = new PrimaryDrawerItem()
+                    .withName(labels.get(i).getTitle())
+                    .withIcon(R.drawable.ic_label)
+                    .withIdentifier(i + 10)// identifier begin from 10
+                    .withCheckable(true);
+            //mDrawer.addItemAtPosition(drawerItem, i + 4);
+        }
+
+
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.
                 OnNavigationItemSelectedListener(){
             @Override
@@ -55,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                         View view1 = View.inflate(MainActivity.this, R.layout.add_layout, null);
                         Button calcle=(Button)view1.findViewById(R.id.add_button_cancle);
                         Button commit=(Button)view1.findViewById(R.id.add_button_commit);
+                        temp_tagText=(EditText)view1.findViewById(R.id.add_tag_edit);
                         alertDialog
                                 .setTitle("添加标签")
                                 .setView(view1)
@@ -63,13 +97,17 @@ public class MainActivity extends AppCompatActivity {
                         calcle.setOnClickListener(new View.OnClickListener(){
                             @Override
                             public void onClick(View v) {
-                                //Toast.makeText(MainActivity.this,"取消",Toast.LENGTH_SHORT).show();
+                                show.dismiss();
                             }
                         });
                         commit.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                //Toast.makeText(MainActivity.this,"确定",Toast.LENGTH_SHORT).show();
+                                Label addLabel=new Label();
+                                String newLabel=temp_tagText.getText().toString();
+                                addLabel.setTitle(newLabel);
+                                LabelLab.get(MainActivity.this).addLabel(addLabel);
+                                navigationView.getMenu().add(2,2,2,newLabel);
                                 show.dismiss();
                             }
                         });
@@ -113,6 +151,7 @@ public class MainActivity extends AppCompatActivity {
     //菜单
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.toolbar,menu);
+
         return true;
     }
     @Override  //顶部菜单的
@@ -150,5 +189,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
 
 }
